5f6268c7e11294bd74916bb194dde5504009b585
taramouse <nickykeyse@gmail.com>
2019-02-19 14:17:10 +0000

    Added dynamic pet data.

--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -1,12 +1,13 @@
 <template>
   <v-container grid-list-md fluid>
     <v-layout wrap>
-      <v-flex xs12 sm4 md3>
+      <v-flex xs12 sm4 md3 v-for="pet in dogs" :key="pet.breed">
         <v-card color="grey lighten-2">
-          <v-img src="https://goo.gl/6CQNDo" height="170px"></v-img>
+          <v-img :src="pet.img" height="170px"></v-img>
           <v-card-title>
             <div>
-              <h3>Looking for a dog?</h3>
+              <h3>{{pet.name}}</h3>
+              <p>{{pet.breed}}</p>
             </div>
           </v-card-title>
         </v-card>
@@ -14,6 +15,19 @@
     </v-layout>
   </v-container>
 </template>
+
+<script>
+import { Dogs } from '../data/dogs'
+
+export default {
+  data() {
+    return {
+      dogs: Dogs
+    }
+  }
+}
+</script>
+
 
 <style scoped>
 p {
